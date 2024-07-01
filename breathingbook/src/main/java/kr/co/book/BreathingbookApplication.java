7a47d83853e7aa342c778ffa01aa234d39cbca43
jeon <jeon_eunmi@naver.com>
2024-07-01 09:51:39 +0900

    setting

---
+++ breathingbook/src/main/java/kr/co/book/BreathingbookApplication.java
@@ -0,0 +1,13 @@
+package kr.co.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreathingbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreathingbookApplication.class, args);
+	}
+
+}
