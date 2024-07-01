6fc19134a55d1afc4ccf68c2436c0f0b71ae9eb4
Mun Sumin <sumin9908@bu.ac.kr>
2024-07-01 17:32:48 +0900

    test2

--- breathingbook/src/main/java/kr/co/book/ecom/controller/jeoncontroller.java
+++ breathingbook/src/main/java/kr/co/book/ecom/controller/jeoncontroller.java
@@ -7,24 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class jeoncontroller {
 
 
-<<<<<<< HEAD
-	@GetMapping("/login")
-	   public String login(){
-	      return "e_login";
-	   }
+
 	
-	@GetMapping("/join")
-	   public String join(){
-=======
 	@GetMapping("/elogin")
-	
 	   public String elogin(){
 	      return "e_login";
 	   }
 	
 	@GetMapping("/ejoin")
 	   public String ejoin(){
->>>>>>> d352e725271621c0de3678735a14ccaea210ea6d
+
 	      return "e_join";
 	   }
 	
