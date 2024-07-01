a3168e4c144f55aa4a2396f332e8be5a8a2c3c5f
jeon <jeon_eunmi@naver.com>
2024-07-01 17:55:42 +0900

    ㅇㅇ

--- breathingbook/src/main/java/kr/co/book/ecom/controller/jeoncontroller.java
+++ breathingbook/src/main/java/kr/co/book/ecom/controller/jeoncontroller.java
@@ -8,14 +8,23 @@ public class jeoncontroller {
 
 
 
+<<<<<<< HEAD
+=======
+	
+
+>>>>>>> parent of 397a776 (Merge branch 'main' of https://github.com/suronicle/SecondProjet)
 	@GetMapping("/elogin")
+	
 	   public String elogin(){
 	      return "e_login";
 	   }
 	
 	@GetMapping("/ejoin")
 	   public String ejoin(){
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 397a776 (Merge branch 'main' of https://github.com/suronicle/SecondProjet)
 	      return "e_join";
 	   }
 	
